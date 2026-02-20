6b7b9f55e2dab54bf1df1e8ea703256d73be379b
Kaan Kuzu <153543986+kaankuzu1@users.noreply.github.com>
2026-02-20 09:37:37 +0300

    Update Cal.com booking URL to new audiencelabs link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const CALENDLY_URL = "https://cal.com/kaan-kuzu-stfjw7/30min";
+export const CALENDLY_URL = "https://cal.com/kaan-kuzu-9ds6ay/audiencelabs";
 
 export const TRUST_BRANDS = [
   "Kick",
